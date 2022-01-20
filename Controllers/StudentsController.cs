c09ff09a12e0770d1dbc36a9561f24d201a714bf
satyamp800 <satyamp800@gmail.com>
2022-01-20 12:42:09 +0530

    Add project files.

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,61 @@
+﻿using CodeFirstCrud.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirstCrud.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private DatabaseContext Context;
+
+        public StudentsController(DatabaseContext common)
+        {
+            Context = common;
+
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await Context.tbl_Student.ToListAsync());
+        }
+        //Get Method code
+        public IActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new Student());
+            else
+                return View(Context.tbl_Student.Find(id));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                if (student.Id ==0)                
+                    Context.Add(student);              
+                else                 
+                    Context.Update(student);
+                await Context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View(student);
+        }
+        //Delete method
+        public async Task<IActionResult> Delete(int? id)
+        {
+            var employee = await Context.tbl_Student.FindAsync(id);
+            Context.tbl_Student.Remove(employee);
+            await Context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirstCrud.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Please enter your anme")]
+        [Column(TypeName = "nvarchar(270)")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Please enter your age")]
+     
+        public int Age { get; set; }
+        [Required(ErrorMessage ="Please enter your collegename")]
+        [Column(TypeName = "nvarchar(270)")]
+        public string Collegename { get; set; }
+        [Required(ErrorMessage ="Please enter your Address")]
+        [Column(TypeName = "nvarchar(270)")]
+        public string Address { get; set; }
+
+    }
+}
